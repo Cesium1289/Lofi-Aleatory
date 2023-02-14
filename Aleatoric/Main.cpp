c6be1ff1dd5a7f398a9b74883c3378c3d4cbccc7
tieph2 <108648438+tieph2@users.noreply.github.com>
2023-02-13 19:03:06 -0800

    Added command line parser

--- Aleatoric/Main.cpp
+++ Aleatoric/Main.cpp
@@ -272,3 +272,87 @@ int CheckKeyValue(int rootKey, char& character)
 	}
 	return 1;
 }
+
+bool ParseArguments(WaveParameters& param, int argc, const char* argv[])
+{
+	string* argvArray = new string[argc];
+	for (size_t i = 0; i < argc; i++)
+	{
+		argvArray[i] = argv[i];
+		cout << argvArray[i];
+	}
+
+	for (size_t i = 1; i < argc; i++) {
+		if (!argvArray[i].find("--root"))
+		{
+			param.setRootKey(ParseSubString(argvArray[i]));
+		}
+
+		else if (!argvArray[i].find("--bpm"))
+		{
+			param.setBPM(ParseSubString(argvArray[i]));
+
+		}
+		else if (!argvArray[i].find("--volume"))
+		{
+			param.setVolume(ParseSubString(argvArray[i]));
+
+		}
+		else if (!argvArray[i].find("--ramp"))
+		{
+			param.setRamp(ParseSubString(argvArray[i]));
+			cout << argvArray[i];
+
+		}
+		else if (!argvArray[i].find("--wave"))
+		{
+			ParseStringToString(argvArray[i]);
+			param.setWaveType(argvArray[i]);
+		}
+		else if (!argvArray[i].find("--scale"))
+		{
+			ParseStringToString(argvArray[i]);
+			param.setScaleType(argvArray[i]);
+		}
+		else if (!argvArray[i].find("--random"))
+		{
+			param.setRandom();
+		}
+		else
+		{
+			cout << "ERORR: invalid argument " << argv[i] << endl;
+			cout << "Closing program... \n";
+		}
+	}
+
+	return true;
+}
+
+float ParseSubString(string& arg)
+{
+	size_t pos = arg.find("[");
+	arg.erase(0, pos + 1);
+	pos = arg.find("]");
+	arg.erase(pos);
+
+	try {
+		return stof(arg);
+	}
+	catch (invalid_argument const& error)
+	{
+		cout << "Invalid parameter value: " << error.what() << endl;
+		return -1.0;
+	}
+}
+
+void ParseStringToString(string& arg)
+{
+	size_t pos = arg.find("[");
+	arg.erase(0, pos + 1);
+	pos = arg.find("]");
+	arg.erase(pos);
+}
+
+bool ParseArguments(WaveParameters& param, int argc, const char* argv[]);
+float ParseSubString(string& arg);
+void ParseStringToString(string& arg);
