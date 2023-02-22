6a974df56cc80bb1fe51ec8b2cb39531125a0b9a
Cesium1289 <Carson.hansen@gmail.com>
2023-02-22 11:09:59 -0800

    Rework WaveGenerator and WaveParameter

--- Aleatoric/WaveGenerator.h
+++ Aleatoric/WaveGenerator.h
@@ -1,4 +1,5 @@
-#pragma once
+#ifndef WaveGenerator_H
+#define WaveGenerator_H
 #include<SFML/Audio.hpp>
 #include "WaveParameters.h"
 using namespace std;
@@ -8,22 +9,18 @@ class WaveGenerator
 public:
 	void playWave(WaveParameters&);
 private:
+	void GenerateSawWave(WaveParameters& params, float freq, sf::SoundBuffer& buffer);
+	void GenerateSineWave(WaveParameters& params, float freq, sf::SoundBuffer& buffer);
+	void GenerateSquareWave(WaveParameters& params, float freq, sf::SoundBuffer& buffer);
+	void GenerateTriangleWave(WaveParameters& params, float freq, sf::SoundBuffer& buffer);
+	float GenerateAmplitude(float volume);
+	void GenerateWave(WaveParameters& params, float freq, sf::SoundBuffer& buffer);
 
 	void CalculateFrequency(float freqArray[], vector<int>& scaleArray, int rootKey);
-
 	float WaveFunc(float pos, int type);
-
-	void GenerateWave(float freq, sf::SoundBuffer& Wave, WaveParameters& args, int waveType);
-
-	void SetAmplitudes(WaveParameters& args);
-
 	void RampSamples(vector<sf::Int16>& sample, float frac);
-	
 	void fillMajorArray(vector<int>& scaleArray, char key);
-
 	int CheckKeyValue(int rootKey, char& character);
-
     void GenerateRandomWave(float frequency, sf::SoundBuffer& Wave);
-
 };
-
+#endif
