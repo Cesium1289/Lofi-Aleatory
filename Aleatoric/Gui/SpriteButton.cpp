a7e5b3270665cf091b768901524e208722b45981
tieph2 <108648438+tieph2@users.noreply.github.com>
2023-03-14 17:15:08 -0700

    Fixed SpriteButton

--- Aleatoric/Gui/SpriteButton.cpp
+++ Aleatoric/Gui/SpriteButton.cpp
@@ -4,169 +4,177 @@
 namespace gui
 {
 
-SpriteButton::SpriteButton(const sf::Texture& texture, const sf::String& string):
-    Widget(),
-    m_pressed(false)
-{
-    size_t width = texture.getSize().x;
-    size_t height = texture.getSize().y / 3; // default, hover, focus
-
-    m_background.setTexture(texture);
-    m_background.setTextureRect(sf::IntRect(0, 0, width, height));
-
-    setSize(width, height);
-
-    m_text.setFont(Theme::getFont());
-    m_text.setCharacterSize(Theme::textSize);
-
-    setString(string);
-}
-
-
-void SpriteButton::setString(const sf::String& string)
-{
-    m_text.setString(string);
-    centerText(m_text);
-}
-
-
-const sf::String& SpriteButton::getString() const
-{
-    return m_text.getString();
-}
-
-
-void SpriteButton::setFont(const sf::Font& font)
-{
-    m_text.setFont(font);
-    centerText(m_text);
-}
-
-
-const sf::Font& SpriteButton::getFont() const
-{
-    return *m_text.getFont();
-}
-
-
-void SpriteButton::setTextSize(size_t size)
-{
-    m_text.setCharacterSize(size);
-    centerText(m_text);
-}
-
-void SpriteButton::setTexture(const sf::Texture& texture)
-{
-    size_t width = texture.getSize().x;
-    size_t height = texture.getSize().y / 3; // default, hover, focus
-
-    m_background.setTexture(texture);
-    m_background.setTextureRect(sf::IntRect(0, 0, width, height));
-}
-
-void SpriteButton::toggleTexture(const sf::Texture& texture) {
-    m_background.setTexture(texture);
-    sf::Vector2f size = getSize();
-    static bool enabled = false;
-    if (enabled)
-        m_background.setTextureRect(sf::IntRect(0, 0, size.x, size.y));
-    else
-        m_background.setTextureRect(sf::IntRect(0, 2*size.y, size.x, 2*size.y));
-}
-
-void SpriteButton::draw(sf::RenderTarget& target, sf::RenderStates states) const
-{
-    states.transform *= getTransform();
-    target.draw(m_background, states);
-    target.draw(m_text, states);
-}
-
-
-// Callbacks -------------------------------------------------------------------
-
-void SpriteButton::onStateChanged(State state)
-{
-    sf::Vector2f size = getSize();
-    switch (state)
+    SpriteButton::SpriteButton(const sf::Texture& defaultTexture, const sf::Texture& activeTexture, const sf::String& string) :
+        Widget(),
+        m_pressed(false)
     {
-    case StateDefault:
-        m_background.setTextureRect(sf::IntRect(0, 0, size.x, size.y));
-        break;
-    case StateHovered:
-        m_background.setTextureRect(sf::IntRect(0, size.y, size.x, size.y));
-        break;
-    case StatePressed:
-        m_background.setTextureRect(sf::IntRect(0, size.y, size.x, size.y));
-    case StateFocused:
-        m_background.setTextureRect(sf::IntRect(0, size.y * 2, size.x, size.y));
-        break;
+        size_t width = defaultTexture.getSize().x;
+        size_t height = defaultTexture.getSize().y / 3; // default, hover, focus
+
+        m_background.setTexture(defaultTexture);
+        m_background.setTextureRect(sf::IntRect(0, 0, width, height));
+
+        setSize(width, height);
+
+        m_text.setFont(Theme::getFont());
+        m_text.setCharacterSize(Theme::textSize);
+
+        textures.first = defaultTexture;
+        textures.second = activeTexture;
+
+        setString(string);
     }
-}
 
-
-void SpriteButton::onMouseMoved(float x, float y)
-{
-    if (isFocused())
+    bool SpriteButton::isActive() const
     {
-        if (containsPoint({x, y}) && sf::Mouse::isButtonPressed(sf::Mouse::Left))
-            press();
+        return m_active;
+    }
+
+
+    void SpriteButton::setString(const sf::String& string)
+    {
+        m_text.setString(string);
+        centerText(m_text);
+    }
+
+
+    const sf::String& SpriteButton::getString() const
+    {
+        return m_text.getString();
+    }
+
+
+    void SpriteButton::setFont(const sf::Font& font)
+    {
+        m_text.setFont(font);
+        centerText(m_text);
+    }
+
+
+    const sf::Font& SpriteButton::getFont() const
+    {
+        return *m_text.getFont();
+    }
+
+
+    void SpriteButton::setTextSize(size_t size)
+    {
+        m_text.setCharacterSize(size);
+        centerText(m_text);
+    }
+
+    void SpriteButton::setTexture(const sf::Texture& texture)
+    {
+        size_t width = texture.getSize().x;
+        size_t height = texture.getSize().y / 3; // default, hover, focus
+
+        m_background.setTexture(texture);
+        m_background.setTextureRect(sf::IntRect(0, 0, width, height));
+    }
+
+    void SpriteButton::toggle() {
+        sf::Vector2f size = getSize();
+        static bool enabled = false;
+        if (m_active)
+            m_background.setTexture(textures.first);
         else
-            release();
+            m_background.setTexture(textures.second);
+        m_active = !m_active;
     }
-}
 
-
-void SpriteButton::onMousePressed(float, float)
-{
-    press();
-}
-
-
-void SpriteButton::onMouseReleased(float x, float y)
-{
-
-    release();
-    if (containsPoint({x, y}))
+    void SpriteButton::draw(sf::RenderTarget& target, sf::RenderStates states) const
     {
-        triggerCallback();
+        states.transform *= getTransform();
+        target.draw(m_background, states);
+        target.draw(m_text, states);
     }
-}
 
 
-void SpriteButton::onKeyPressed(const sf::Event::KeyEvent& key)
-{
-    if (key.code == sf::Keyboard::Return)
+    // Callbacks -------------------------------------------------------------------
+
+    void SpriteButton::onStateChanged(State state)
     {
-        triggerCallback();
+        sf::Vector2f size = getSize();
+        switch (state)
+        {
+        case StateDefault:
+            m_background.setTextureRect(sf::IntRect(0, 0, size.x, size.y));
+            break;
+        case StateHovered:
+            m_background.setTextureRect(sf::IntRect(0, size.y, size.x, size.y));
+            break;
+        case StatePressed:
+            m_background.setTextureRect(sf::IntRect(0, size.y, size.x, size.y));
+        case StateFocused:
+            m_background.setTextureRect(sf::IntRect(0, size.y * 2, size.x, size.y));
+            break;
+        }
+    }
+
+
+    void SpriteButton::onMouseMoved(float x, float y)
+    {
+        if (isFocused())
+        {
+            if (containsPoint({ x, y }) && sf::Mouse::isButtonPressed(sf::Mouse::Left))
+                press();
+            else
+                release();
+        }
+    }
+
+
+    void SpriteButton::onMousePressed(float, float)
+    {
         press();
     }
-}
 
 
-void SpriteButton::onKeyReleased(const sf::Event::KeyEvent& key)
-{
-    if (key.code == sf::Keyboard::Return)
+    void SpriteButton::onMouseReleased(float x, float y)
+    {
+
         release();
-}
-
-
-void SpriteButton::press()
-{
-    if (!m_pressed)
-    {
-        m_pressed = true;
-        m_text.move(0, 1);
+        if (containsPoint({ x, y }))
+        {
+            triggerCallback();
+        }
     }
-}
 
 
-void SpriteButton::release()
-{
-    if (m_pressed)
+    void SpriteButton::onKeyPressed(const sf::Event::KeyEvent& key)
     {
-        m_pressed = false;
-        m_text.move(0, -1);
+        if (key.code == sf::Keyboard::Return)
+        {
+            triggerCallback();
+            press();
+        }
     }
-}
+
+
+    void SpriteButton::onKeyReleased(const sf::Event::KeyEvent& key)
+    {
+        if (key.code == sf::Keyboard::Return)
+            release();
+    }
+
+
+    void SpriteButton::press()
+    {
+        if (!m_pressed)
+        {
+            m_pressed = true;
+            m_text.move(0, 1);
+        }
+    }
+
+
+    void SpriteButton::release()
+    {
+        if (m_pressed)
+        {
+            m_pressed = false;
+            m_text.move(0, -1);
+        }
+    }
 
 }
--- Aleatoric/Gui/SpriteButton.hpp
+++ Aleatoric/Gui/SpriteButton.hpp
@@ -2,46 +2,52 @@
 #define GUI_SPRITEBUTTON_HPP
 
 #include "Widget.hpp"
+#include <utility>
+using namespace std;
 
 namespace gui
 {
 
-/**
- * Push button linked to a sprite sheet
- */
-class SpriteButton: public Widget
-{
-public:
-    SpriteButton(const sf::Texture& texture, const sf::String& label = "");
+    /**
+     * Push button linked to a sprite sheet
+     */
+    class SpriteButton : public Widget
+    {
+    public:
+        SpriteButton(const sf::Texture& defaultTexture, const sf::Texture& activeTexture, const sf::String& label = "");
 
-    void setString(const sf::String& string);
-    const sf::String& getString() const;
+        bool isActive() const;
 
-    void setFont(const sf::Font& font);
-    const sf::Font& getFont() const;
+        void setString(const sf::String& string);
+        const sf::String& getString() const;
 
-    void setTextSize(size_t size);
+        void setFont(const sf::Font& font);
+        const sf::Font& getFont() const;
 
-    void setTexture(const sf::Texture& texture);
+        void setTextSize(size_t size);
 
-    void toggleTexture(const sf::Texture&);
+        void setTexture(const sf::Texture& texture);
 
-    void onStateChanged(State state) override;
-    void onMouseMoved(float x, float y) override;
-    void onMousePressed(float x, float y) override;
-    void onMouseReleased(float x, float y) override;
-    void onKeyPressed(const sf::Event::KeyEvent& key) override;
-    void onKeyReleased(const sf::Event::KeyEvent& key) override;
+        void toggle();
 
-private:
-    void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
-    void press();
-    void release();
+        void onStateChanged(State state) override;
+        void onMouseMoved(float x, float y) override;
+        void onMousePressed(float x, float y) override;
+        void onMouseReleased(float x, float y) override;
+        void onKeyPressed(const sf::Event::KeyEvent& key) override;
+        void onKeyReleased(const sf::Event::KeyEvent& key) override;
 
-    sf::Text m_text;
-    sf::Sprite m_background;
-    bool m_pressed;
-};
+    private:
+        void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
+        void press();
+        void release();
+
+        sf::Text m_text;
+        sf::Sprite m_background;
+        pair<sf::Texture, sf::Texture> textures;
+        bool m_pressed;
+        bool m_active;
+    };
 
 }
 
