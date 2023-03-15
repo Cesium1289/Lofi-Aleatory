09ea89728c1acda7c8ad56a823528a65249b2943
Cesium1289 <Carson.hansen@gmail.com>
2023-03-14 20:37:09 -0700

    Removed unneeded test

--- UnitTests/UnitTests.cpp
+++ UnitTests/UnitTests.cpp
@@ -11,19 +11,6 @@ namespace UnitTests
 	{
 	public:
 		
-		TEST_METHOD(TestAddMethod)
-		{
-			WaveParameters WP;
-			int a = 2;
-			int b = 2;
-			int expected = 4;
-
-			// Actual value
-			int actual = WP.TestAddMethod(a, b);
-			
-			Assert::AreEqual(expected, actual, L" Expected Sum : 4 ", LINE_INFO());
-		}
-
 		TEST_METHOD(TestSimpleAdd)
 		{
 			int testA = 2;
